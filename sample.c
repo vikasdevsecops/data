27b659c7c9b8b6e4cf3f4ed34123bcf66a766ac8
sathyadevops <sathyadevops1@gmail.com>
2020-01-04 11:28:01 +0530

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,3 +1,14 @@
+void getData()
+{
+  printf(" Get data");
+  printf(" Get data");  
+}
+void putData()
+{
+  printf(" Put data");
+  printf(" Put data");  
+}
+
 void main(
   printf(" test code");
   printf(" test code");
